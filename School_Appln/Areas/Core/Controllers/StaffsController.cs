8e96c3b3d1837120919a2b436711234713abff40
appletechd <ddoshi@cosmolex.com>
2018-03-19 11:20:50 +0530

    AddEducationalQualificationDetails() method added

--- School_Appln/Areas/Core/Controllers/StaffsController.cs
+++ School_Appln/Areas/Core/Controllers/StaffsController.cs
@@ -130,7 +130,7 @@ namespace School_Appln.Areas.Core.Controllers
 					//    return RedirectToAction("Index");
 					case "Save & Continue":
 						TempData["Staff_Id"] = staff.Staff_Id;
-						return RedirectToAction("CreateStudentOther");
+						return RedirectToAction("CreateStaffOtherDetails");
 					default:
 						return RedirectToAction("Index");
 				}
@@ -142,5 +142,290 @@ namespace School_Appln.Areas.Core.Controllers
 			ViewBag.Staff_Id = new SelectList(db.Staff_Type, "Staff_Type_Id", "Staff_Type_Name");
 			return View(staff);
 		}
+
+
+	    //Save & Continue
+		public ActionResult CreateStaffOtherDetails()
+		{
+			//string ForOtherDetailStaffId = TempData.Peek("Staff_Id").ToString();
+			return View();
+		}
+
+		#region Add Educational Qualification Details 
+		[HttpPost]
+		public JsonResult AutoCompleteQualification(string qual_Name)
+		{
+
+			var qualificationList = (from qual in db.Qualifications
+								   where qual.Qualification_Name.StartsWith(qual_Name)
+								   select new
+								   {
+									   label = qual.Qualification_Name,
+									   val = qual.Qualification_Id
+								   }).ToList();
+
+			return Json(qualificationList, JsonRequestBehavior.AllowGet);
+		}
+
+		[HttpPost]
+		public JsonResult AutoCompleteSpecialization(string specialization_Name)
+		{
+			var specializationList = (from specialization in db.Specializations
+									 where specialization.Specialization_Name.StartsWith(specialization_Name)
+									 select new
+									 {
+										 label = specialization.Specialization_Name,
+										 val = specialization.Specialization_Id
+									 }).ToList();
+
+			return Json(specializationList, JsonRequestBehavior.AllowGet);
+		}
+
+		[HttpPost]
+		public JsonResult AutoCompleteInstitution(string institution_Name)
+		{
+			var institutionList = (from institution in db.Institutions
+									  where institution.Institution_Name.StartsWith(institution_Name)
+									  select new
+									  {
+										  label = institution.Institution_Name,
+										  val = institution.Institution_Id
+									  }).ToList();
+
+			return Json(institutionList, JsonRequestBehavior.AllowGet);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> AddEducationalQualificationDetails(string staffId, string qualification_Id, string specialization_Id, string institution_Id, string passingYear, string MediumOfInstruction)
+		{
+			var userId = LoggedInUser.Id;
+			JsonResult result = new JsonResult();
+			try
+			{
+				//var schoolid = Convert.ToInt32(schoolId);
+				var staff_Id = Convert.ToInt32(staffId);
+				var qualificationId = Convert.ToInt32(qualification_Id);
+				var specializationId = Convert.ToInt32(specialization_Id);
+				var institutionId = Convert.ToInt32(institution_Id);
+				var passing_Year = Convert.ToInt64(passingYear);
+				var Medium_Of_Instruction =MediumOfInstruction;
+				//   , string FrmYear, string ToYear, string Leaving, string Comments
+
+				var CheckStaffQualDetails = db.Staff_Educational_Details.Where(a => a.Staff_Id == staff_Id  && a.Qualification_Id == qualificationId).Count();
+
+				if (CheckStaffQualDetails == 0)
+				{
+					Staff_Educational_Details addStaffEduQualDetails = new Staff_Educational_Details();
+					addStaffEduQualDetails.Staff_Id = staff_Id;
+					addStaffEduQualDetails.Qualification_Id = qualificationId;
+					addStaffEduQualDetails.Specialization_Id = specializationId;
+					addStaffEduQualDetails.Institution_Id = institutionId;
+					addStaffEduQualDetails.Is_Deleted = false;
+					addStaffEduQualDetails.Year_Of_Passing = passing_Year;
+					addStaffEduQualDetails.Medium_Of_Instruction = Medium_Of_Instruction;
+					addStaffEduQualDetails.Academic_Year = (DateTime.Now.Month < 3) ? DateTime.Now.Year - 1 : DateTime.Now.Year;
+					//addStaffWorkExpDetails.Comments = Comments;
+					addStaffEduQualDetails.Created_On = DateTime.Now;
+					addStaffEduQualDetails.Created_By = userId;
+					addStaffEduQualDetails.Is_Active = true;
+
+					db.Staff_Educational_Details.Add(addStaffEduQualDetails);
+					await db.SaveChangesAsync();
+				}
+				else
+				{
+
+				}
+			}
+			catch (Exception ex)
+			{
+				result.Data = new { Result = "ERROR", Message = " This property already available in this block " };
+			}
+
+			return RedirectToAction("CreateStaffOtherDetails");
+		}
+
+		public JsonResult GetEducationalQualificationDetails(string sidx, string sord, int page, int rows, string staffId)
+		{
+			var refStaffId = Convert.ToInt64(staffId);
+			int pageIndex = Convert.ToInt32(page) - 1;
+			int pageSize = rows;
+			var EduQualList = (from stEduQualDetail in db.Staff_Educational_Details
+								   join st in db.Staffs on stEduQualDetail.Staff_Id equals st.Staff_Id
+								   join institution in db.Institutions on stEduQualDetail.Institution_Id equals institution.Institution_Id
+								   join specialization in db.Specializations on stEduQualDetail.Specialization_Id equals specialization.Specialization_Id
+								   join qualification in db.Qualifications on stEduQualDetail.Qualification_Id equals qualification.Qualification_Id
+							   where stEduQualDetail.Staff_Id == refStaffId && stEduQualDetail.Is_Deleted != true
+								   select new { stEduQualDetail.Staff_Id, stEduQualDetail.Qualification_Id, stEduQualDetail.Specialization_Id , stEduQualDetail.Institution_Id,stEduQualDetail.Academic_Year, qualification.Qualification_Name ,specialization.Specialization_Name,institution.Institution_Name, stEduQualDetail.Year_Of_Passing, stEduQualDetail.Medium_Of_Instruction, stEduQualDetail.Created_By, stEduQualDetail.Created_On }
+							  ).ToList();
+			int totalRecords = EduQualList.Count();
+			var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+			if (sord.ToUpper() == "DESC")
+			{
+				EduQualList = EduQualList.OrderByDescending(s => s.Staff_Id).ToList();
+				EduQualList = EduQualList.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+			}
+			else
+			{
+				EduQualList = EduQualList.OrderBy(s => s.Staff_Id).ToList();
+				EduQualList = EduQualList.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+			}
+			var jsonData = new
+			{
+				total = totalPages,
+				page,
+				records = totalRecords,
+				rows = EduQualList
+			};
+
+			return Json(jsonData, JsonRequestBehavior.AllowGet);
+
+		}
+
+		public async Task<ActionResult> DeleteEduQualDetails(string staffId, string qual_Id)
+		{
+			var userId = LoggedInUser.Id;
+			JsonResult result = new JsonResult();
+			try
+			{
+				var staff_Id = Convert.ToInt32(staffId);
+				var qualId = Convert.ToInt32(qual_Id);
+				Staff_Educational_Details deleteEduQualDetails = db.Staff_Educational_Details.Where(a => a.Staff_Id == staff_Id && a.Qualification_Id == Convert.ToInt32(qual_Id)).FirstOrDefault();
+				db.Entry(deleteEduQualDetails).CurrentValues.SetValues(deleteEduQualDetails);
+				deleteEduQualDetails.Is_Deleted = true;
+				db.Entry(deleteEduQualDetails).State = EntityState.Modified;
+				await db.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				result.Data = new { Result = "ERROR", Message = " This property already available in this block " };
+			}
+
+			return RedirectToAction("CreateStaffOtherDetails");
+		}
+		#endregion
+
+		#region Add Previous Work Experience Details
+		[HttpPost]
+		public JsonResult AutoCompleteDesignation(string DesignationName)
+		{
+			var designationList = (from designation in db.Designations
+							  where designation.Designation_Name.StartsWith(DesignationName)
+							  select new
+							  {
+								  label = designation.Designation_Name,
+								  val = designation.Designation_Id
+							  }).ToList();
+
+			return Json(designationList, JsonRequestBehavior.AllowGet);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> AddPrevWorkExpDetails(string schoolId, string Staff_Id, string Name, string Address,string Designation, string FrmYear, string ToYear, string Subjects_Handled, string Designation_Id)
+		{
+			var userId = LoggedInUser.Id;
+			JsonResult result = new JsonResult();
+			try
+			{
+				var schoolid = Convert.ToInt32(schoolId);
+				var staffId = Convert.ToInt32(Staff_Id);
+				var designation = Convert.ToInt32(Designation_Id);
+				var frmYear = Convert.ToInt32(FrmYear);
+				var toYear = Convert.ToInt32(ToYear);
+				//   , string FrmYear, string ToYear, string Leaving, string Comments
+
+				var CheckStudent = db.Staff_Exp_Details.Where(a => a.Staff_Id == staffId && a.School_Id == schoolid).Count();
+
+				if (CheckStudent == 0)
+				{
+					Staff_Exp_Details addStaffWorkExpDetails = new Staff_Exp_Details();
+					addStaffWorkExpDetails.Staff_Id = staffId;
+					addStaffWorkExpDetails.School_Id = schoolid;
+					addStaffWorkExpDetails.Designation_Id = Convert.ToInt32(Designation_Id);					
+					addStaffWorkExpDetails.From_Year = frmYear;
+					addStaffWorkExpDetails.Is_Deleted = false;
+					addStaffWorkExpDetails.To_Year = toYear;
+					addStaffWorkExpDetails.Subject_Id = Subjects_Handled;
+					addStaffWorkExpDetails.Academic_Year =(DateTime.Now.Month < 3 )? DateTime.Now.Year - 1 : DateTime.Now.Year;
+					//addStaffWorkExpDetails.Comments = Comments;
+					addStaffWorkExpDetails.Created_On = DateTime.Now;
+					addStaffWorkExpDetails.Created_By = userId;
+					addStaffWorkExpDetails.Is_Active = true;
+
+					db.Staff_Exp_Details.Add(addStaffWorkExpDetails);
+					await db.SaveChangesAsync();
+				}
+				else
+				{
+
+				}
+			}
+			catch (Exception ex)
+			{
+				result.Data = new { Result = "ERROR", Message = " This property already available in this block " };
+			}
+
+			return RedirectToAction("CreateStaffOtherDetails");
+		}
+
+		public JsonResult GetPrevWorkExpDetails(string sidx, string sord, int page, int rows, string staffId)
+		{
+			var refStaffId = Convert.ToInt64(staffId);
+			int pageIndex = Convert.ToInt32(page) - 1;
+			int pageSize = rows;
+			var PrevWorkExpList = (from stexpdetail in db.Staff_Exp_Details
+							  join st in db.Staffs on stexpdetail.Staff_Id equals st.Staff_Id
+							  join schl in db.Schools on stexpdetail.School_Id equals schl.Id
+							  join desg in db.Designations on stexpdetail.Designation_Id equals desg.Designation_Id
+							  where stexpdetail.Staff_Id == refStaffId && stexpdetail.Is_Deleted != true
+							  select new { stexpdetail.Staff_Id, stexpdetail.School_Id, stexpdetail.Academic_Year,schl.Name, desg.Designation_Name ,stexpdetail.From_Year, stexpdetail.To_Year, stexpdetail.Subject_Id, stexpdetail.Created_By, stexpdetail.Created_On }
+							  ).ToList();
+			int totalRecords = PrevWorkExpList.Count();
+			var totalPages = (int)Math.Ceiling((float)totalRecords / (float)rows);
+			if (sord.ToUpper() == "DESC")
+			{
+				PrevWorkExpList = PrevWorkExpList.OrderByDescending(s => s.Staff_Id).ToList();
+				PrevWorkExpList = PrevWorkExpList.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+			}
+			else
+			{
+				PrevWorkExpList = PrevWorkExpList.OrderBy(s => s.Staff_Id).ToList();
+				PrevWorkExpList = PrevWorkExpList.Skip(pageIndex * pageSize).Take(pageSize).ToList();
+			}
+			var jsonData = new
+			{
+				total = totalPages,
+				page,
+				records = totalRecords,
+				rows = PrevWorkExpList
+			};
+
+			return Json(jsonData, JsonRequestBehavior.AllowGet);
+
+		}
+
+		public async Task<ActionResult> DeleteWorkExpDetails(string schoolId, string StaffId)
+		{
+			var userId = LoggedInUser.Id;
+			JsonResult result = new JsonResult();
+			try
+			{
+				var school_Id = Convert.ToInt32(schoolId);
+				var staff_Id = Convert.ToInt32(StaffId);
+				Staff_Exp_Details deleteWorkExpDetails = db.Staff_Exp_Details.Where(a => a.School_Id == school_Id && a.Staff_Id == staff_Id).FirstOrDefault();
+				db.Entry(deleteWorkExpDetails).CurrentValues.SetValues(deleteWorkExpDetails);
+				deleteWorkExpDetails.Is_Deleted = true;
+				db.Entry(deleteWorkExpDetails).State = EntityState.Modified;
+				await db.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				result.Data = new { Result = "ERROR", Message = " This property already available in this block " };
+			}
+
+			return RedirectToAction("CreateStaffOtherDetails");
+		}
+		#endregion
 	}
+
 }
