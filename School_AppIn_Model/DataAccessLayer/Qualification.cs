df2cf4c816a0128c7ccd7fa2b636ca786e32ba4a
appletechd <ddoshi@cosmolex.com>
2018-03-19 11:25:31 +0530

    Added Qualifications and Specializations class

---
+++ School_AppIn_Model/DataAccessLayer/Qualification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace School_AppIn_Model.DataAccessLayer
+{
+	[Table("Qualification")]
+	public partial class Qualification
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Qualification_Id { get; set; }
+
+		//[StringLength(200)]
+		public string Qualification_Name { get; set; }
+	}
+}
---
+++ School_AppIn_Model/DataAccessLayer/Specialization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace School_AppIn_Model.DataAccessLayer
+{
+	[Table("Specialization")]
+	public partial class Specialization
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Specialization_Id { get; set; }
+
+		[StringLength(50)]
+		public string Specialization_Name { get; set; }
+	}
+}
